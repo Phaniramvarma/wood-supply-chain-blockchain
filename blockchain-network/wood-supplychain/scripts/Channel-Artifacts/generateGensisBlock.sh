e3af2ef4d493d7b504c4f64f2bc73ba26218c375
Phaniramvarma <69008724+Phaniramvarma@users.noreply.github.com>
2020-12-24 21:38:55 +0530

    Update

--- blockchain-network/wood-supplychain/scripts/Channel-Artifacts/generateGensisBlock.sh
+++ blockchain-network/wood-supplychain/scripts/Channel-Artifacts/generateGensisBlock.sh
@@ -1,5 +1,5 @@
 #Next we need to generate genesis block for orderer. It can be done by using configtxgen script
 # generate orderer.block on config directory
 
-export FABRIC_CFG_PATH=/home/ubuntu/blockchain-network/wood-supplychain/fabric-config/
+export FABRIC_CFG_PATH=../../fabric-config/
 ../../bin/configtxgen -profile MultiOrgsOrdererGenesis -outputBlock ../../channel-artifacts/genesis.block -channelID system-channel
